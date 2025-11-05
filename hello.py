97c29de0856e2a9f389a2bfe7a211305899f92d8
mdh.danial <mdh.danialhaziq@gmail.com>
2025-11-05 16:09:13 +0800

    Initial version

---
+++ hello.py
@@ -0,0 +1,11 @@
+# testing git with python
+
+# variables (1st commit)
+name = input("What is your name my friend?")
+print(f"Hello, {name}")
+
+# conditionals (2nd commit)
+
+# loops (3rd commit)
+
+# functions (4th commit)
