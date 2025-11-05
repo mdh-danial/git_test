5bed08916103372ca7d5869758e632aaf4ee3150
mdh.danial <mdh.danialhaziq@gmail.com>
2025-11-05 16:25:14 +0800

    Added conditionals

--- hello.py
+++ hello.py
@@ -2,9 +2,17 @@
 
 # variables (1st commit)
 name = input("What is your name my friend?")
-print(f"Hello, {name}")
 
 # conditionals (2nd commit)
+try:
+    check_name = str(name)
+except ValueError:
+    print("There's no way this will print, right?")
+
+if check_name.lower() == "world":
+    print("Is that you, world?")
+else:
+    print(f"Nevermind. Hello, {name}")
 
 # loops (3rd commit)
 
