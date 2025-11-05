2cb6f385aabd8f2fb7efd26ba6f5ec67d3c46375
mdh.danial <mdh.danialhaziq@gmail.com>
2025-11-05 16:36:51 +0800

    Added a loop to spell name

--- hello.py
+++ hello.py
@@ -15,5 +15,12 @@ else:
     print(f"Nevermind. Hello, {name}")
 
 # loops (3rd commit)
+print("Your name is spelt:", end=" ")
+n = len(check_name)
+for i in range(n):
+    if i == n - 1:
+        print(check_name[i])
+    else:
+        print(check_name[i], end="-")
 
 # functions (4th commit)
